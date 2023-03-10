892c703b50d1e64f7ec4d1760c0baf325433b664
WhyNotSantosh <san_sriramadasu@yahoo.com>
2023-03-10 08:07:11 +0530

    added comments

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const App = () => {
   return (
     <>
     <div style={{textAlign:"center", fontSize:"30px", padding: "5px" , color:"indigo", fontWeight:"bold", backgroundColor:"ivory"}}>Lighthouse - Web Vitals</div>
-    <img src={lighthouse} alt="" style={{maxWidth:"100%"}}/>
+    {/* <img src={lighthouse} alt="" style={{maxWidth:"100%"}}/> -- This line will decrease performance as it has large size image and no width and height to the image*/}
+    <img src={compressedLighthouse} alt="" style={{maxWidth:"100%", width:"600", height:"600"}}/> {/*This is more optimized */}
     </>
   )
 }
